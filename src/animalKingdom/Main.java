f77b7788cd25702152aa1525e84d71ceb6f09353
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-19 21:51:06 -0700

    sorting by lungs and 1758

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -15,21 +15,21 @@ public class Main {
     public static void main(String[] args) {
 
         Mammal panda = new Mammal("Panda", 1869);
-        Mammal zebra = new Mammal("Zebra", 1869);
-        Mammal koala = new Mammal("Koala", 1869);
-        Mammal sloth = new Mammal("Sloth", 1869);
-        Mammal armadillo = new Mammal("Armadillo", 1869);
-        Mammal raccoon = new Mammal("Raccoon", 1869);
-        Mammal bigfoot = new Mammal("Bigfoot", 1869);
+        Mammal zebra = new Mammal("Zebra", 1778);
+        Mammal koala = new Mammal("Koala", 1816);
+        Mammal sloth = new Mammal("Sloth", 1804);
+        Mammal armadillo = new Mammal("Armadillo", 1758);
+        Mammal raccoon = new Mammal("Raccoon", 1758);
+        Mammal bigfoot = new Mammal("Bigfoot", 2021);
 
         Bird pigeon = new Bird("Pigeon", 1837);
-        Bird peacock = new Bird("Peacock", 1837);
-        Bird toucan = new Bird("Toucan", 1837);
-        Bird parrot = new Bird("Parrot", 1837);
-        Bird swan = new Bird("Swan", 1837);
+        Bird peacock = new Bird("Peacock", 1821);
+        Bird toucan = new Bird("Toucan", 1758);
+        Bird parrot = new Bird("Parrot", 1824);
+        Bird swan = new Bird("Swan", 1758);
 
         Fish salmon = new Fish("Salmon", 1758);
-        Fish catfish = new Fish("Catfish", 1758);
+        Fish catfish = new Fish("Catfish", 1817);
         Fish perch = new Fish("Perch", 1758);
 
         ArrayList<AbstractAnimal> animalList = new ArrayList<AbstractAnimal>(Arrays.asList(panda, zebra, koala, sloth, armadillo, raccoon, bigfoot, pigeon, peacock, toucan, parrot, swan, salmon, catfish, perch));
@@ -62,6 +62,9 @@ public class Main {
         }
         System.out.println("*** ANIMALS WITH LUNGS ***");
         printAnimals(animalList, a->a.breath().equals("Lungs"));
+
+        System.out.println("*** LUNGS AND 1758 ***");
+        printAnimals(animalList, a-> (a.breath().equals("Lungs")) && (a.getYear()==1758));
 
 
 
