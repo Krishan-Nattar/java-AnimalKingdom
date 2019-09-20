9a7f94ad8402958f29ae15d70bc3555c5f5b3076
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-19 21:59:27 -0700

    Stretch complete

--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -41,36 +41,49 @@ public class Main {
         for (AbstractAnimal a : animalList) {
             System.out.println(a.getName() + " " + a.getYear());
         }
-        
+        System.out.println();
         animalList.sort((a1,a2)-> a1.getName().compareToIgnoreCase(a2.getName()));
         System.out.println("*** ORDERING BY NAME ALPHABETICALLY ***");
         for (AbstractAnimal a : animalList) {
             System.out.println(a.getName());
         }
 
+        System.out.println();
         animalList.sort((a1,a2)-> a1.move().compareToIgnoreCase(a2.move()));
         System.out.println("*** ORDERING BY MOVEMENT ***");
         for (AbstractAnimal a : animalList) {
             System.out.println(a.getName() + " " + a.move());
         }
 
-
+        System.out.println();
         animalList.sort((a1,a2)-> a1.breath().compareToIgnoreCase(a2.breath()));
         System.out.println("*** ORDERING BY BREATHING ***");
         for (AbstractAnimal a : animalList) {
             System.out.println(a.getName() + " " + a.breath());
         }
+        System.out.println();
         System.out.println("*** ANIMALS WITH LUNGS ***");
         printAnimals(animalList, a->a.breath().equals("Lungs"));
 
+        System.out.println();
         System.out.println("*** LUNGS AND 1758 ***");
         printAnimals(animalList, a-> (a.breath().equals("Lungs")) && (a.getYear()==1758));
 
+        System.out.println();
         System.out.println("*** LUNGS AND EGGS ***");
         printAnimals(animalList, a-> (a.breath().equals("Lungs")) && (a.reproduce().equals("Eggs")));
 
+        System.out.println();
         System.out.println("*** 1758 ***");
         printAnimals(animalList, a-> (a.getYear()==1758));
+
+        //Ordering alphabetically
+        
+        System.out.println();
+        System.out.println("*** Alphabetical and Lungs ***");
+        animalList.sort((a1,a2)-> a1.getName().compareToIgnoreCase(a2.getName()));
+        printAnimals(animalList, a-> (a instanceof Mammal));
+        
 
     }
 }
