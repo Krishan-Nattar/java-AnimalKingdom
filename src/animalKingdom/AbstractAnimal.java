013e2deb15b9b21dfc98007de2ab3926eb962c8f
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-19 21:16:52 -0700

    refactor

--- src/animalKingdom/AbstractAnimal.java
+++ src/animalKingdom/AbstractAnimal.java
@@ -1,63 +1,37 @@
 package animalKingdom;
 
-public abstract class AbstractAnimal implements Animal
-{
+public abstract class AbstractAnimal implements Animal {
     private int id;
     private static int maxId;
     private String name;
     private int year;
 
-
-    public AbstractAnimal(String name, int year)
-    {
+    public AbstractAnimal(String name, int year) {
         maxId++;
-        this.id = maxId;        
+        this.id = maxId;
         this.name = name;
         this.year = year;
     }
 
-    public String eat()
-    {
+    public String eat() {
         return "Munch";
     }
 
-    public int getId()
-    {
+    public int getId() {
         return this.id;
     }
 
-    public String getName()
-    {
+    public String getName() {
         return this.name;
     }
 
-    public int getYear()
-    {
+    public int getYear() {
         return this.year;
     }
 
     @Override
-    public String toString()
-    {
-        return this.name + " " +
-        this.year + "\n";
+    public String toString() {
+        return this.name + " " + this.year + "\n";
     }
-
-    
-
-//     All animals consume food the same way
-
-// Each animal is assigned a unique number, a name, and year discovered regardless of classification.
-
-// Methods will return a string saying how that animal implements the action
-
-// All animals can move, breath, reproduce. How they do that, so what string should get returned when the method is executed, varies by animal type.
-
-
-
-
-
-
-
 
 }
--- src/animalKingdom/Main.java
+++ src/animalKingdom/Main.java
@@ -2,57 +2,43 @@ package animalKingdom;
 
 import java.util.*;
 
-public class Main
-{
-    public static void printVehicles(ArrayList<AbstractAnimal> animals, CheckAnimal tester)
-    {
-        for(AbstractAnimal a: animals)
-        {
-            if(tester.test(a))
-            {
+public class Main {
+    public static void printVehicles(ArrayList<AbstractAnimal> animals, CheckAnimal tester) {
+        for (AbstractAnimal a : animals) {
+            if (tester.test(a)) {
                 System.out.println();
             }
 
         }
     }
+
     public static void main(String[] args) {
-    
-    Mammal panda = new Mammal("Panda", 1869);
-    Mammal zebra = new Mammal("Zebra", 1869);
-    Mammal koala = new Mammal("Koala", 1869);
-    Mammal sloth = new Mammal("Sloth", 1869);
-    Mammal armadillo = new Mammal("Armadillo", 1869);
-    Mammal raccoon = new Mammal("Raccoon", 1869);
-    Mammal bigfoot = new Mammal("Bigfoot", 1869);
 
+        Mammal panda = new Mammal("Panda", 1869);
+        Mammal zebra = new Mammal("Zebra", 1869);
+        Mammal koala = new Mammal("Koala", 1869);
+        Mammal sloth = new Mammal("Sloth", 1869);
+        Mammal armadillo = new Mammal("Armadillo", 1869);
+        Mammal raccoon = new Mammal("Raccoon", 1869);
+        Mammal bigfoot = new Mammal("Bigfoot", 1869);
 
-    Bird pigeon = new Bird("Pigeon", 1837);
-    Bird peacock = new Bird("Peacock", 1837);
-    Bird toucan = new Bird("Toucan", 1837);
-    Bird parrot = new Bird("Parrot", 1837);
-    Bird swan = new Bird("Swan", 1837);
+        Bird pigeon = new Bird("Pigeon", 1837);
+        Bird peacock = new Bird("Peacock", 1837);
+        Bird toucan = new Bird("Toucan", 1837);
+        Bird parrot = new Bird("Parrot", 1837);
+        Bird swan = new Bird("Swan", 1837);
 
-    Fish salmon = new Fish("Salmon", 1758);
-    Fish catfish = new Fish("Catfish", 1758);
-    Fish perch = new Fish("Perch", 1758);
+        Fish salmon = new Fish("Salmon", 1758);
+        Fish catfish = new Fish("Catfish", 1758);
+        Fish perch = new Fish("Perch", 1758);
 
-    ArrayList <AbstractAnimal> animalList = new ArrayList<AbstractAnimal>(
-        Arrays.asList(panda,zebra,koala,sloth,armadillo,raccoon,bigfoot,pigeon,peacock,toucan,
-        parrot,swan,salmon,catfish,perch));
+        ArrayList<AbstractAnimal> animalList = new ArrayList<AbstractAnimal>(Arrays.asList(panda, zebra, koala, sloth, armadillo, raccoon, bigfoot, pigeon, peacock, toucan, parrot, swan, salmon, catfish, perch));
 
-        animalList.sort((a1,a2)-> a1.getYear() - a2.getYear());
+        animalList.sort((a1, a2) -> a1.getYear() - a2.getYear());
 
+        for (AbstractAnimal a : animalList) {
+            System.out.println(a);
+        }
 
-    // System.out.println(panda.breath());
-    // System.out.println(panda.move());
-    // System.out.println(panda.reproduce());
-    // System.out.println(panda.eat());
-    // System.out.println(panda.getId());
-    for(AbstractAnimal a: animalList)
-    {
-        System.out.println(a);
-    }
-    
-    
     }
 }
